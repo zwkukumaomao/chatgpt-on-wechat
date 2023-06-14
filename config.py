8de880b04273741ca1a973fac60545d9503f6a19
ltm <ltmnnn@163.com>
2023-06-14 16:12:20 +0800

    schedule snd msg

--- config.py
+++ config.py
@@ -102,7 +102,8 @@ available_setting = {
     # 知识库平台配置
     "use_linkai": False,
     "linkai_api_key": "",
-    "linkai_app_code": ""
+    "linkai_app_code": "",
+    "schedule_msg_snd_usernick": ""
 }
 
 
